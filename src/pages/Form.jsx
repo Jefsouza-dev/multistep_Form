262a02aa9767b739db6566dc0b0ad425d7de9067
Jeferson Gonçalves <jefsouza.dev@gmail.com>
2023-05-12 15:15:29 -0300

    feat: install firebase

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -45,7 +45,7 @@ const Form = () => {
         <h2>Deixe sua avaliação</h2>
         <p>
           Ficamos felizes com a sua compra, ultilize o formulário abaixo para
-          avaliar o produto
+          avaliar o produto.
         </p>
       </div>
 
